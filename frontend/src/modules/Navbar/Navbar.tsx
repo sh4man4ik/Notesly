5a039100b1896b6dfadc63898711301a3e9415dc
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-01-12 16:53:19 +0200

    Added automatic language selection

--- frontend/src/modules/Navbar/Navbar.tsx
+++ frontend/src/modules/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import TEXTS from '../../shared/texts/texts.ts';
+import getText from '../../shared/texts/texts.ts';
 
 function Navbar() {
 	return (
 		<>
 			<div className="navbar bg-base-100 shadow-sm">
-				<a className="btn btn-ghost text-xl">{TEXTS.navbar.title}</a>
+				<a className="btn btn-ghost text-xl">{getText('navbar.title')}</a>
 			</div>
 		</>
 	);
--- frontend/src/shared/texts/texts.ts
+++ frontend/src/shared/texts/texts.ts
@@ -1,7 +1,35 @@
-const TEXTS = {
+const EN_TEXTS = {
 	navbar: {
 		title: 'Notesly'
 	}
 };
 
-export default TEXTS;
+function getText(path: string) {
+	let language = navigator.language.slice(0, 2);
+
+	let TEXTS: any;
+
+	switch (language) {
+		case 'en':
+			TEXTS = EN_TEXTS;
+			break;
+		/* Here you can add other languages */
+		default:
+			TEXTS = EN_TEXTS;
+			break;
+	}
+
+	let parts = path.split('.');
+
+	for (let i = 0; i < parts.length; i++) {
+		TEXTS = TEXTS[parts[i]];
+
+		if (!TEXTS) {
+			break;
+		}
+	}
+
+	return TEXTS;
+}
+
+export default getText;
