dd87acbe50f46fedd0a93b4edc522dba5c86f960
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-01-25 12:11:24 +0200

    Notes height updates

--- frontend/src/pages/NotesPage/NotesPage.tsx
+++ frontend/src/pages/NotesPage/NotesPage.tsx
@@ -4,7 +4,7 @@ import Notes from '../../modules/Notes';
 function NotesPage() {
 	return (
 		<>
-			<div className="h-screen flex flex-col">
+			<div className="h-dvh flex flex-col">
 				<Navbar></Navbar>
 				<Notes></Notes>
 			</div>
