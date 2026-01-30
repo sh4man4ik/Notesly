9faf168f8621b09a6ab18ea24837e11acf93fe09
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-01-30 15:16:17 +0200

    Some bog fixes

--- frontend/src/modules/Notes/Notes.tsx
+++ frontend/src/modules/Notes/Notes.tsx
@@ -10,11 +10,16 @@ function Notes() {
 
 	const modules = {
 		toolbar: [
-			[{ header: [1, 2, 3, false] }],
-			['bold', 'italic', 'underline'],
-			['link'],
-			[{ list: 'ordered' }, { list: 'bullet' }],
-			['clean']
+			[
+				{ header: [1, 2, 3, false] },
+				'bold',
+				'italic',
+				'underline',
+				'link',
+				{ list: 'ordered' },
+				{ list: 'bullet' },
+				'clean'
+			]
 		]
 	};
 
